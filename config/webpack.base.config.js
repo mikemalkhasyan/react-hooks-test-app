d27c63d93ee6c727cda0841724716f45da980e8d
Michael <mkrtich.malkhasyan1996@gmail.com>
2019-02-03 21:18:17 +0400

    Add html webpack plugin in webpack config file.

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
         rules: [
@@ -10,4 +12,10 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
 };
