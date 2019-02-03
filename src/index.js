69b6b0f8f367cfa5bebe9517b5517ca46489ce36
Michael <mkrtich.malkhasyan1996@gmail.com>
2019-02-03 22:00:05 +0400

    Add new scss file and include it in react script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./myStyles.scss";
 
 const App = () => {
     return (
-        <div>
+        <div className="app">
             <p>
                 We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
                 have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
