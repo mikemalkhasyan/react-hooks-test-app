a59f6894b436df74cfe3380dc5c1381393083d52
Michael <mkrtich.malkhasyan1996@gmail.com>
2019-02-03 22:33:44 +0400

    Get access to env config variables, use webpack-merge package for having only one webpack configuration file.

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,25 +1,35 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+
+module.exports = env => {
+    return merge([{
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']  // read this from RIGHT to LEFT
                 }
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']  // read this from RIGHT to LEFT
-            }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: './index.html'
+            }),
+            new webpack.DefinePlugin({
+                'process.env.VERSION': JSON.stringify(env.VERSION),
+                'process.env.PLATFORM': JSON.stringify(env.PLATFORM),
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
-        })
-    ]
+    }]);
 };
