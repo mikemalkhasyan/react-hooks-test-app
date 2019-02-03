cd6692927bf555b9434dbf73f58470c7ef8d9b38
Michael <mkrtich.malkhasyan1996@gmail.com>
2019-02-03 23:32:28 +0400

    Change react class component to new functional syntax.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import './myStyles.scss';
 
-class App extends React.Component {
-    state = {
-        CaptainKirkBio: {},
-    };
+function App() {
+    const [captainKirkBio, setCaptainKirkBio] = useState({});
 
-    componentDidMount() {
-        this.onGetKirkBio();
-    }
-
-    onGetKirkBio = async () => {
+    const onGetKirkBio = async () => {
         try {
             const result = await fetch('http://stapi.co/api/v1/rest/character/search', {
                 method: 'POST',
@@ -26,33 +20,34 @@ class App extends React.Component {
             });
             const resultJSON = await result.json();
             const character = resultJSON.characters[0];
-            this.setState({ CaptainKirkBio: character });
+            setCaptainKirkBio( character );
         } catch (error) {
             console.log('error', error);
         }
     };
 
-    render() {
-        const { CaptainKirkBio } = this.state;
-        return (
-            <div className="app">
-                <img alt="header" src="/dist/images/header.jpeg" className="app-header" />
-                <p>
-                    We are a most promising species, Mr. Spock, as predators go. Did you know that? I
-                    frequently have my doubts. I dont. Not any more. And maybe in a thousand years or so, we
-                    will be able to prove it.
-                </p>
-                <p>- Captain Kirk</p>
-                <section>
-                    {Object.values(CaptainKirkBio).length === 0 ? (
-                        <p>Loading User Information</p>
-                    ) : (
-                        <p style={{ wordBreak: 'break-all' }}>{JSON.stringify(CaptainKirkBio)}</p>
-                    )}
-                </section>
-            </div>
-        );
-    }
+    useEffect(() => {
+        onGetKirkBio();
+    });
+
+    return (
+        <div className="app">
+            <img alt="header" src="/dist/images/header.jpeg" className="app-header" />
+            <p>
+                We are a most promising species, Mr. Spock, as predators go. Did you know that? I
+                frequently have my doubts. I dont. Not any more. And maybe in a thousand years or so, we
+                will be able to prove it.
+            </p>
+            <p>- Captain Kirk</p>
+            <section>
+                {Object.values(captainKirkBio).length === 0 ? (
+                    <p>Loading User Information</p>
+                ) : (
+                    <p style={{ wordBreak: 'break-all' }}>{JSON.stringify(captainKirkBio)}</p>
+                )}
+            </section>
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
